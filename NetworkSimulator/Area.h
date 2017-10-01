6a50f36a88806e5f94edf23f000ad2f9b045e1dd
Takuma Matsumoto <t.m515621@gmail.com>
2017-10-01 15:13:45 +0900

    * コンストラクタを追加

--- NetworkSimulator/Area.h
+++ NetworkSimulator/Area.h
@@ -4,6 +4,10 @@ namespace ns{
 	class Area{
 		geo::Rectangle<double> m_rect;
 	public:
+		Area(const geo::Point<double>& left_bottom, const double width, const double height) : 
+			m_rect(left_bottom, width, height){
+
+		}
 		Area(const geo::Rectangle<double>& rect) : m_rect(rect){
 		}
 	};
