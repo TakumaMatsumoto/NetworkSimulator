7ad6d75a5cd5dbd5fd8c206bdab149a783a92b6c
Takuma Matsumoto <t.m515621@gmail.com>
2017-10-05 23:37:28 +0900

    * 初期化パラメータを付けるようにした

--- NetworkSimulator/Config.h
+++ NetworkSimulator/Config.h
@@ -8,29 +8,32 @@
 namespace ns{
 	class Config{
 	public:
-		Config() : m_area(geo::Rectangle<double>(geo::Point<double>(0.0, 0.0), 0.0, 0.0)){}
+		Config(){
+		}
 		enum class RoutingProtocol{
 			EPIDEMIC_ROUTING = 1,
 			SPRAY_AND_WAIT = 2
 		};
 		// シミュレーションエリア[m^2]
-		Area m_area;
+		Area m_area = geo::Rectangle<double>(geo::Point<double>(0.0, 0.0), 0.0, 0.0);
 		// シミュレーション時間[s]
-		int m_simulation_period;
-		// リレーノードの移動速度[m/s]
-		int min, max;
+		int m_simulation_period = 0;
+		// リレーノードの最高移動速度[m/s]
+		int m_max_move_speed = 0;
+		// リレーノードの最低移動速度[m/s]
+		int m_min_move_speed = 0;
 		// 通信速度[bps]
-		int m_transmission_speed;
+		int m_transmission_speed = 0;
 		// 通信可能範囲[m]
-		double m_transmission_range;
+		double m_transmission_range = 0.0;
 		// メッセージサイズ[bit]
-		int m_message_size;
+		int m_message_size = 0;
 		// メッセージ送信間隔[s]
-		int m_message_interval;
+		int m_message_interval = 0;
 		// リレーノード数[個]
-		int m_number_of_relay_nodes;
+		int m_number_of_relay_nodes = 0;
 		// ルーティングプロトコル
-		RoutingProtocol m_routing_protocol;
+		RoutingProtocol m_routing_protocol = RoutingProtocol::EPIDEMIC_ROUTING;
 		static Config createFromTable(const table::Table& tbl, const table::LineHeader& lhead);
 	};
 	class Configs{
