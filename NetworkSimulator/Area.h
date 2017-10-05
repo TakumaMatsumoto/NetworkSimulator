ac31e84a488b87433e30389d714bff58220a1540
Takuma Matsumoto <t.m515621@gmail.com>
2017-10-05 11:37:06 +0900

    * アクセサを追加

--- NetworkSimulator/Area.h
+++ NetworkSimulator/Area.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "geo.h"
 
 namespace ns{
 	class Area{
 		geo::Rectangle<double> m_rect;
 	public:
+		double getWidth() const{
+			return m_rect.width;
+		}
+		double getHeight() const{
+			return m_rect.height;
+		}
 		Area(const geo::Point<double>& left_bottom, const double width, const double height) : 
 			m_rect(left_bottom, width, height){
 
