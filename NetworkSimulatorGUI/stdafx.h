0e27332563625039cff3e3d60e757101615ff810
Takuma Matsumoto <t.m515621@gmail.com>
2017-10-05 12:50:21 +0900

    * プリコンパイル済みヘッダーに必要なヘッダファイルを追加

--- NetworkSimulatorGUI/stdafx.h
+++ NetworkSimulatorGUI/stdafx.h
@@ -1,5 +1,8 @@
+#include <wx/aui/aui.h>
 #include <wx/wx.h>
 #include <wx/sizer.h>
+#include <wx/notebook.h>
+#include <wx/listctrl.h>
 #include <memory>
 #include <string>
 #include <unordered_map>
