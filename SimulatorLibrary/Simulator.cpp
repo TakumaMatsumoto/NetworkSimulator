77e1da4469bf808a60ea62a018cab9bccd5580a2
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-12 13:35:46 +0900

    * DLLに関するエラー処理を追加

--- SimulatorLibrary/Simulator.cpp
+++ SimulatorLibrary/Simulator.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <memory>
 #include <functional>
+#include <algorithm>
 #include "Simulator.h"
 using namespace sim;
 using CFUNC = sim::IRunnable*(*)(const table::RowHeader& row_header, const table::Table& tbl);
@@ -16,7 +17,10 @@ void Simulator::run() {
 		}
 		return ret;
 	}(m_conf);
-
+	if (sim_lib_handle == NULL || std::any_of(result_lib_handlers.begin(), result_lib_handlers.end(), [](const HMODULE& handle) { return handle == NULL; })) {
+		MessageBoxA(NULL, "指定したdllファイルが見つかりませんでした", "error", MB_ICONERROR);
+		return;
+	}
 
 	const CFUNC CreateSimulationInstance = reinterpret_cast<CFUNC>(GetProcAddress(sim_lib_handle, "CreateSimulatorInstance"));
 	const std::vector<RFUNC> OutputResultFunctions = [](const std::vector<HMODULE>& result_lib_handlers) {
@@ -27,7 +31,10 @@ void Simulator::run() {
 		}
 		return ret;
 	}(result_lib_handlers);
-
+	if (CreateSimulationInstance == NULL || std::any_of(OutputResultFunctions.begin(), OutputResultFunctions.end(), [](const RFUNC& func) { return func == NULL; })) {
+		MessageBoxA(NULL, "関数の呼び出しに失敗しました", "error", MB_ICONERROR);
+		return;
+	}
 
 	const auto row_heads = m_param_table.getRowHeaders();
 	for (const auto& row_head : row_heads)
