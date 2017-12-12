8f2174ffdf6c68c86ce1ef671c487cbcc64f007b
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-13 00:30:40 +0900

    * 外部公開用DLL関数の名前を変更

--- NetworkLibrary/SimpleWirelessNetwork/DLLFunctions.h
+++ NetworkLibrary/SimpleWirelessNetwork/DLLFunctions.h
@@ -2,4 +2,4 @@
 #include "IRunnable.h"
 #include "Table.h"
 
-sim::IRunnable* CreateSimulatorInstance(const std::unordered_map<std::string, std::string>& param_map);
+sim::IRunnable* CreateSimulationInstance(const std::unordered_map<std::string, std::string>& param_map);
