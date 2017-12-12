75685ebd318dc3025e9aa613a8c0303b17cb2cc0
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-12 13:21:19 +0900

    * Simulator::runをリファクタリング

--- SimulatorLibrary/Simulator.cpp
+++ SimulatorLibrary/Simulator.cpp
@@ -3,37 +3,46 @@
 #include <functional>
 #include "Simulator.h"
 using namespace sim;
+using CFUNC = sim::IRunnable*(*)(const table::RowHeader& row_header, const table::Table& tbl);
+using RFUNC = void(*)(const std::unordered_map<std::string, std::string>& result);
 
 void Simulator::run() {
-	HMODULE sim_lib_handle_module = LoadLibraryA(m_conf.m_simulation_dll_filename.c_str());
-	using CFUNC = sim::IRunnable*(*)(const table::RowHeader& row_header, const table::Table& tbl);
-	auto CreateSimulationInstance = reinterpret_cast<CFUNC>(GetProcAddress(sim_lib_handle_module, "CreateSimulatorInstance"));
+	const HMODULE sim_lib_handle = LoadLibraryA(m_conf.m_simulation_dll_filename.c_str());
+	const std::vector<HMODULE> result_lib_handlers = [](const Config& conf) {
+		std::vector<HMODULE> ret;
+		for (const auto& result_output_dll_filename : conf.m_output_dlls_filename)
+		{
+			ret.push_back(LoadLibraryA(result_output_dll_filename.c_str()));
+		}
+		return ret;
+	}(m_conf);
 
-	std::vector<HMODULE> result_handle_modules;
-	for (const auto& result_output_dll_filename : m_conf.m_output_dlls_filename)
-	{
-		result_handle_modules.push_back(LoadLibraryA(result_output_dll_filename.c_str()));
-	}
-	using RFUNC = void(*)(const std::unordered_map<std::string, std::string>& result);
-	std::vector<RFUNC> result_output_functions;
-	for (const auto& result_handle_module : result_handle_modules)
-	{
-		result_output_functions.push_back(reinterpret_cast<RFUNC>(GetProcAddress(result_handle_module, "OutputResult")));
-	}
+
+	const CFUNC CreateSimulationInstance = reinterpret_cast<CFUNC>(GetProcAddress(sim_lib_handle, "CreateSimulatorInstance"));
+	const std::vector<RFUNC> OutputResultFunctions = [](const std::vector<HMODULE>& result_lib_handlers) {
+		std::vector<RFUNC> ret;
+		for (const auto& result_handle_module : result_lib_handlers)
+		{
+			ret.push_back(reinterpret_cast<RFUNC>(GetProcAddress(result_handle_module, "OutputResult")));
+		}
+		return ret;
+	}(result_lib_handlers);
+
+
 	const auto row_heads = m_param_table.getRowHeaders();
 	for (const auto& row_head : row_heads)
 	{
 		const auto simulation = std::unique_ptr<sim::IRunnable>(CreateSimulationInstance(row_head, m_param_table));
 		simulation->run();
-		for (const auto& result_output_function : result_output_functions)
+		for (const auto& OutputResult : OutputResultFunctions)
 		{
-			result_output_function(simulation->toMap());
+			OutputResult(simulation->toMap());
 		}
 	}
 
-	FreeLibrary(sim_lib_handle_module);
-	for (const auto& result_handle_module : result_handle_modules)
+	FreeLibrary(sim_lib_handle);
+	for (const auto& result_lib_handle : result_lib_handlers)
 	{
-		FreeLibrary(result_handle_module);
+		FreeLibrary(result_lib_handle);
 	}
 }
