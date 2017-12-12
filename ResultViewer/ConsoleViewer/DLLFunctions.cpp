cbf5ea98625637e2c7aac7cd4709d93e8a90b642
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-12 16:11:08 +0900

    * コンソールに結果を表示するdllプロジェクトを追加

---
+++ ResultViewer/ConsoleViewer/DLLFunctions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "DLLFunctions.h"
+using namespace std;
+
+void OutputResult(const std::unordered_map<std::string, std::string>& result) {
+
+}
---
+++ ResultViewer/ConsoleViewer/DLLFunctions.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+void OutputResult(const std::unordered_map<std::string, std::string>& result);
