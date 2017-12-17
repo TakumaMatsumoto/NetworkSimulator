1a603f86b1ad4e1ffa8385b04ea8d34577dc689e
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-17 09:47:07 +0900

    * Windows用シミュレータークラスを追加 * DLLからシミュレーション、結果出力クラスの生成制御を呼び出すようにした

---
+++ Simulator/MSW/MSWSimulator.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include "Simulator.h"
+#include "MSWSimulator.h"
+using namespace sim::msw;
+
+int Simulator::run() const {
+	const auto env_factory = new sim::RunnableEnvironmentFactory(
+		new DLLInstanceFactory<ISimulation>(m_conf.m_simulation_dll_filename),
+		new DLLInstanceFactory<IViewer>(m_conf.m_output_dlls_filename[0]));
+	const sim::Simulator simulator(
+		env_factory, 
+		m_conf.m_sim_config,
+		std::make_pair(0, -1),
+		[](const std::exception& ex) {
+		MessageBoxA(NULL, ex.what(), "error", MB_OK | MB_ICONERROR);
+	});
+	return simulator.run();
+}
---
+++ Simulator/MSW/MSWSimulator.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <Windows.h>
+#include "Table.h"
+#include "Config.h"
+#include "ISimulation.h"
+#include "IViewer.h"
+#include "MSWConfig.h"
+#include "IFactory.h"
+
+namespace sim
+{
+	namespace msw {
+		class Simulator{
+		private:
+			const msw::Config m_conf;
+			/*
+				DLLから関数を呼び出してインスタンスを生成する担当
+			*/
+			template<typename T>
+			class DLLInstanceFactory : public IFactory<T> {
+			private:
+				HMODULE m_lib_handle;
+				const std::string m_dll_filename;
+				// インスタンス生成のDLL関数
+				// @param
+				// param_map: パラメータのハッシュマップ(key: パラメータ名, value: パラメータ値(string))
+				using CREATE_INSTANCE_FUNC = T*(*)(const std::unordered_map<std::string, std::string>& param_map);
+				CREATE_INSTANCE_FUNC m_func;
+			public:
+				DLLInstanceFactory(const std::string& dll_filename) : m_dll_filename(dll_filename) {
+
+				}
+				T* createInstance(const std::unordered_map<std::string, std::string>& param_map) override {
+					return m_func(param_map);
+				}
+				void init() override {
+					m_lib_handle = LoadLibraryA(m_dll_filename.c_str());
+					if (m_lib_handle == NULL) {
+						throw std::exception(std::string(m_dll_filename + "\n" + "DLL file for simulation not found").c_str());
+					}
+					m_func = reinterpret_cast<CREATE_INSTANCE_FUNC>(GetProcAddress(m_lib_handle, "CreateInstance"));
+					if (m_func == NULL) {
+						throw std::exception(std::string(m_dll_filename + "\n" + "CreateInstance function not found").c_str());
+					}
+				}
+				void terminate() override {
+					if (m_lib_handle != NULL) FreeLibrary(m_lib_handle);
+				}
+			};
+		public:
+			// シミュレータのインスタンス
+			// @param conf:シミュレーター全体に関わる設定
+			Simulator(const msw::Config& conf) : m_conf(conf){
+			}
+			int run() const;
+		};
+	}
+}
