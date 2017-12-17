09dd35701a7f8b02643bbfc80b25e7635e5c8fbd
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-17 09:42:12 +0900

    * インターフェース名を変更

--- SimulatorLibrary/ISimulation.h
+++ SimulatorLibrary/ISimulation.h
@@ -1,15 +1,12 @@
 #pragma once
 #include <unordered_map>
-#include <string>
-#include "IRunnable.h"
 
 namespace sim
 {
 	class ISimulation {
 	public:
 		virtual ~ISimulation() = default;
-		// シミュレーションのインスタンスを作成します
-		// @param param_map: パラメータ名をkey,値をvalueとしたハッシュマップ
-		virtual sim::IRunnable* createInstance(const std::unordered_map<std::string, std::string>& param_map) = 0;
+		// return: ハッシュ値にした実行結果
+		virtual std::unordered_map<std::string, std::string> run() = 0;
 	};
 }
