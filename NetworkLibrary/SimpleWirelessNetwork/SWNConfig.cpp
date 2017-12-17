380d108277fc0e3220cb1f493d768391b01327b7
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-17 13:18:53 +0900

    * シミュレーションのパラメータを追加

--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
@@ -3,10 +3,11 @@ using namespace sim::swn;
 
 Config Config::createFromMap(const std::unordered_map<std::string, std::string>& umap) {
 	Config ret;
-	ret.m_simulation_period = std::stoul(umap.at("simulation_period"));
-	ret.m_number_of_nodes	= std::stoul(umap.at("number_of_nodes"));
-	ret.m_size_of_data		= std::stoul(umap.at("size_of_data"));
-	ret.m_area_width		= std::stoul(umap.at("area_width"));
-	ret.m_area_height		= std::stoul(umap.at("area_height"));
+	ret.m_area_width			= std::stoul(umap.at("area_width"));
+	ret.m_area_height			= std::stoul(umap.at("area_height"));
+	ret.m_number_of_nodes		= std::stoul(umap.at("number_of_nodes"));
+	ret.m_transmission_range	= std::stoul(umap.at("transmission_range"));
+	ret.m_message_size			= std::stoul(umap.at("message_size"));
+	ret.m_initial_energy		= std::stoul(umap.at("initial_energy"));
 	return ret;
 }
--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
@@ -5,16 +5,18 @@ namespace sim {
 	namespace swn {
 		class Config {
 		public:
-			// シミュレーション実行時間
-			unsigned int m_simulation_period;
-			// ノード数
-			unsigned int m_number_of_nodes;
-			// 1つのノードが収集するデータ数
-			unsigned int m_size_of_data;
 			// エリア幅
 			unsigned int m_area_width;
 			// エリア高さ
 			unsigned int m_area_height;
+			// ノード数
+			unsigned int m_number_of_nodes;
+			// 通信可能範囲
+			unsigned int m_transmission_range;
+			// 1つのノードが収集するデータ数[bit]
+			unsigned int m_message_size;
+			// 初期エネルギー[J]
+			unsigned int m_initial_energy;
 			static Config createFromMap(const std::unordered_map<std::string, std::string>& umap);
 		};
 	}
