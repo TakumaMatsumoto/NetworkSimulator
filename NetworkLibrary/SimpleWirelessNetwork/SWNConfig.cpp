05547311d7e02c9328c5392eee845b663221a4cf
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-20 12:30:53 +0900

    * 設定項目を追加

--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
@@ -29,13 +29,14 @@ void split(const std::string& s, const std::string& delim, List& result)
 
 Config Config::createFromMap(const std::unordered_map<std::string, std::string>& umap) {
 	Config ret;
-	ret.m_area_width			= std::stoul(umap.at("area_width"));
-	ret.m_area_height			= std::stoul(umap.at("area_height"));
-	ret.m_number_of_nodes		= std::stoul(umap.at("number_of_nodes"));
-	ret.m_transmission_range	= std::stoul(umap.at("transmission_range"));
-	ret.m_message_size			= std::stoul(umap.at("message_size"));
-	ret.m_initial_energy		= std::stoul(umap.at("initial_energy"));
-
+	ret.m_area_width				= std::stoul(umap.at("area_width"));
+	ret.m_area_height				= std::stoul(umap.at("area_height"));
+	ret.m_number_of_sensor_nodes	= std::stoul(umap.at("number_of_sensor_nodes"));
+	ret.m_transmission_range		= std::stoul(umap.at("transmission_range"));
+	ret.m_message_size				= std::stoul(umap.at("message_size"));
+	ret.m_initial_energy			= std::stoul(umap.at("initial_energy"));
+	ret.m_consume_energy_for_communication_per_bit = std::stod(umap.at("Eelec"));
+	ret.m_consume_energy_for_transmission_per_bit  = std::stod(umap.at("Emp"));
 	std::vector<std::string> result;
 	split(umap.at("rails"), "|", result);
 	for (const auto& s : result)
--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
@@ -9,17 +9,25 @@ namespace sim {
 			unsigned int m_area_width;
 			// エリア高さ
 			unsigned int m_area_height;
-			// ノード数
-			unsigned int m_number_of_nodes;
+			// センサノード数
+			unsigned int m_number_of_sensor_nodes;
 			// 通信可能範囲
 			unsigned int m_transmission_range;
 			// 1つのノードが収集するデータ数[bit]
 			unsigned int m_message_size;
 			// 初期エネルギー[J]
 			unsigned int m_initial_energy;
+			// 1bitのデータを通信する際の消費電力[J/bit]
+			double m_consume_energy_for_communication_per_bit;
+			// 1bitのデータを1mだけ送信する際に使用される増幅器の消費電力[J/(bit・m^2)]
+			double m_consume_energy_for_transmission_per_bit;
 			// レールの角度
 			std::vector<double> m_rails;
 			static Config createFromMap(const std::unordered_map<std::string, std::string>& umap);
+			// エリアの範囲を取得する
+			std::pair<unsigned int, unsigned int> getAreaRange() const {
+				return std::make_pair(m_area_width, m_area_height);
+			}
 		};
 	}
 }
