bc65a27fe17f5cec86e06add6f618e3719d11a2a
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-24 10:03:16 +0900

    * オブジェクトの初期化方法を変更 * 乱数の生成式を修正

--- NetworkLibrary/SimpleWirelessNetwork/Simulation.cpp
+++ NetworkLibrary/SimpleWirelessNetwork/Simulation.cpp
@@ -1,13 +1,14 @@
 #include "Simulation.h"
 #include "Table.h"
 #include "Result.h"
-#include "Node.h"
+#include "UIDCreator.h"
 using namespace sim::swn;
-#define NRAND() static_cast<double>(rand()) / INT_MAX
+#define NRAND() static_cast<double>(rand()) / RAND_MAX
 
-std::unordered_map<std::string, std::string> Simulation::run() {
+Simulation::Object Simulation::createObject() {
+	UIDCreator uid_creator;
 	// 初期化処理
-	SensorNodes sensor_nodes;
+	std::vector<std::shared_ptr<SensorNode>> snodes;
 	// センサノードを配置
 	for (unsigned int i = 0; i < m_conf.m_number_of_sensor_nodes; i++)
 	{
@@ -16,24 +17,57 @@ std::unordered_map<std::string, std::string> Simulation::run() {
 			m_conf.m_area_height * NRAND());
 		const auto battery = Battery(m_conf.m_initial_energy);
 		const auto antenner = Antenner(m_conf.m_consume_energy_for_communication_per_bit, m_conf.m_consume_energy_for_transmission_per_bit);
-		sensor_nodes.add(std::make_shared<SensorNode>(point, m_conf.m_transmission_range, battery, antenner));
+		snodes.push_back(std::make_shared<SensorNode>(uid_creator.get(), point, m_conf.m_transmission_range, battery, antenner));
 	}
-	BaseNode base_node(geo::Point<double>(0.0, 0.0));
-	// シミュレーションの空間情報
-	const auto space = Space(Message(m_conf.m_message_size));
+	// 基地局(ベースノード)を配置
+	const auto base_node = std::make_shared<BaseNode>(uid_creator.get(), geo::Point<double>(0.0, 0.0));
+	// センサノードの配列を基地局に近い順にする
+	const auto sort_func = [&](const std::shared_ptr<SensorNode>& lhs, const std::shared_ptr<SensorNode>& rhs) {
+		return lhs->getPosition().distanceTo(base_node->getPosition()) < rhs->getPosition().distanceTo(base_node->getPosition());
+	};
+	std::sort(snodes.begin(), snodes.end(), sort_func);
+	SensorNodes sensor_nodes(snodes);
+	// マップ情報の設定
+	const auto nodes = [&]() {
+		auto ret = sensor_nodes.getNodes();
+		ret.push_back(base_node);
+		return ret;
+	}();
+	const auto p_gmap = std::make_shared<GeometryMap>(Message(m_conf.m_message_size), m_conf.getRails(uid_creator), nodes);
+	base_node->equip(p_gmap);
+	sensor_nodes.equip(p_gmap);
+
+	return Object(sensor_nodes, base_node, p_gmap);
+}
+
+std::vector<std::weak_ptr<INode>> Simulation::Object::getNodes() const {
+	auto ret = mp_sensor_nodes.getNodes();
+	ret.push_back(mp_base_node);
+	return ret;
+}
+
+std::unordered_map<std::string, std::string> Simulation::run() {
+	auto obj = createObject();
 	// 一つのノードが死ぬまでにかかるラウンド数を数える
 	unsigned int round = 0;
-	while (sensor_nodes.areAlive())
+	while (obj.mp_sensor_nodes.areAlive())
 	{
+		// 地図情報を更新する
+		obj.mp_gmap->update();
 		// 全センサノードがデータのセンシングを行う
-		sensor_nodes.sensing(space);
+		obj.mp_sensor_nodes.sensing();
 		// 全センサノードが送信先センサノードを探す(移動含)
-		sensor_nodes.searchReceiver(sensor_nodes.getNodes());
+		obj.mp_sensor_nodes.searchReceiver();
+		// 全センサノードが送信元センサノードを探す
+		obj.mp_sensor_nodes.searchSender();
 		// ベースノードが収集元のノードを探す
-		base_node.searchSender(sensor_nodes.getNodes());
+		obj.mp_base_node->searchSender();
 		// 収集元のノードからメッセージを取得する
-		base_node.collectMessage();
+		const auto p_msg = obj.mp_base_node->collectMessage();
+		unsigned int size = p_msg->getSize();
+		// 全センサノードが初期位置へ戻る
+		obj.mp_sensor_nodes.returnToInitialPosition();
 		round++;
 	}
-	return Result(round, 0.0, 0.0, 0.0).toMap();
+	return Result(round, 0, 0.0, 0.0).toMap();
 }
--- NetworkLibrary/SimpleWirelessNetwork/Simulation.h
+++ NetworkLibrary/SimpleWirelessNetwork/Simulation.h
@@ -2,11 +2,25 @@
 #include <unordered_map>
 #include "ISimulation.h"
 #include "SWNConfig.h"
+#include "Node.h"
 
 namespace sim {
 	namespace swn {
 		class Simulation : public ISimulation {
 			const swn::Config m_conf;
+			class Object {
+			public:
+				SensorNodes mp_sensor_nodes;
+				std::shared_ptr<BaseNode> mp_base_node;
+				std::shared_ptr<GeometryMap> mp_gmap;
+				Object(const SensorNodes& p_sensor_nodes, const std::shared_ptr<BaseNode>& p_base_node, const std::shared_ptr<GeometryMap>& p_gmap) :
+					mp_sensor_nodes(p_sensor_nodes), mp_base_node(p_base_node), mp_gmap(p_gmap) {
+
+				}
+				std::vector<std::weak_ptr<INode>> getNodes() const;
+			};
+			// シミュレーションに必要なオブジェクトを生成する
+			Object createObject();
 		public:
 			Simulation(const std::unordered_map<std::string, std::string>& param_map) 
 				: m_conf(Config::createFromMap(param_map)) {
