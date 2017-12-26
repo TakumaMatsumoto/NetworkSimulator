7032335fdb98806f44c791dafc018b0c23aa31d3
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-26 13:24:30 +0900

    * CSVで保存する機能を実装中

--- SimulationObserver/CSVRecorder/CSVRecorder.h
+++ SimulationObserver/CSVRecorder/CSVRecorder.h
@@ -1,14 +1,57 @@
 #pragma once
 #include "ISimulationObserver.h"
 #include "Table.h"
+template <typename List>
+void split(const std::string& s, const std::string& delim, List& result)
+{
+	result.clear();
+
+	using string = std::string;
+	string::size_type pos = 0;
+
+	while (pos != string::npos)
+	{
+		string::size_type p = s.find(delim, pos);
+
+		if (p == string::npos)
+		{
+			result.push_back(s.substr(pos));
+			break;
+		}
+		else {
+			result.push_back(s.substr(pos, p - pos));
+		}
+
+		pos = p + delim.size();
+	}
+}
 
 namespace sim {
 	namespace observer {
 		class CSVRecorder : public ISimulationObserver {
+		private:
+			class Config {
+			public:
+				const bool m_log = false;
+				const std::string m_filename;
+				Config(const std::string& filename) : m_filename(filename) {
+				}
+				static Config createFromConfigFile() {
+					const std::string fname = "config.txt";
+					std::vector<std::string> result;
+					split(, "=", result);
+
+				}
+			};
+			const Config m_conf;
+			table::Table m_table;
 		public:
+			CSVRecorder(const Config& conf) : m_conf(conf) {
+
+			}
 			// シミュレータ開始時に実行される関数
 			// @param conf: シミュレータの設定
-			virtual void onSimulatorBegin(const Config& conf) override {
+			virtual void onSimulatorBegin(const sim::Config& conf) override {
 
 			}
 			// ある一つのシミュレーション設定下でのシミュレーションが開始するタイミングで実行される関数
@@ -32,11 +75,32 @@ namespace sim {
 			// @param param_name:	シミュレーション設定の名前
 			// @param results: 試行回数分のシミュレーション結果
 			virtual void onSimulationsEnd(const std::string& param_name, const std::vector<std::unordered_map<std::string, std::string>>& results) override {
-
+				const auto chead = table::ColumnHeader(param_name);
+				const auto keys = [&results]() {
+					std::vector<std::string> ret;
+					for (const auto& map : results[0])
+					{
+						ret.push_back(map.first);
+					}
+					return ret;
+				}();
+				// 各キーの値を取得して平均値を算出する
+				for (const auto& key : keys)
+				{
+					const auto rhead = table::RowHeader(key);
+					double ave = 0.0;
+					for (const auto& result : results)
+					{
+						ave += std::stod(result.at(key));
+					}
+					m_table.insert(
+						table::Index(chead, rhead), 
+						table::Cell(std::to_string(ave)));
+				}
 			}
 			// シミュレータ終了時に実行される関数
 			virtual void onSimulatorEnd() override {
-
+				table::FileStorage(m_conf.m_filename).save(m_table);
 			}
 		};
 	}
