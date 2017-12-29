5e2b5aa87370b893a7a88e809fc07b8793334dea
Takuma Matsumoto <t.m515621@gmail.com>
2017-12-29 11:56:45 +0900

    * パラメータ変更に伴い、パラメータをプログラム側で読み込めるようにした

--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.cpp
@@ -29,19 +29,31 @@ void split(const std::string& s, const std::string& delim, List& result)
 
 Config Config::createFromMap(const std::unordered_map<std::string, std::string>& umap) {
 	Config ret;
-	ret.m_area_width				= std::stoul(umap.at("area_width"));
-	ret.m_area_height				= std::stoul(umap.at("area_height"));
-	ret.m_number_of_sensor_nodes	= std::stoul(umap.at("number_of_sensor_nodes"));
-	ret.m_transmission_range		= std::stoul(umap.at("transmission_range"));
-	ret.m_message_size				= std::stoul(umap.at("message_size"));
-	ret.m_initial_energy			= std::stoul(umap.at("initial_energy"));
-	ret.m_consume_energy_for_communication_per_bit = std::stod(umap.at("Eelec"));
-	ret.m_consume_energy_for_transmission_per_bit  = std::stod(umap.at("Emp"));
-	std::vector<std::string> result;
-	split(umap.at("rails"), "|", result);
-	for (const auto& s : result)
+	// 地形に関する情報
+	ret.m_area_width				 = std::stoul(umap.at("area_width"));
+	ret.m_area_height				 = std::stoul(umap.at("area_height"));
+	ret.m_number_of_sensor_nodes	 = std::stoul(umap.at("number_of_sensor_nodes"));
 	{
-		ret.m_rail_angles.push_back(std::stod(s));
+		std::vector<std::string> result;
+		split(umap.at("rails"), "|", result);
+		for (const auto& s : result)
+		{
+			ret.m_rail_angles.push_back(std::stod(s));
+		}
 	}
+	// ノードに関する情報
+	ret.m_transmission_range.first	 = std::stod(umap.at("minimum_transmission_range"));
+	ret.m_transmission_range.second  = std::stod(umap.at("maximum_transmission_range"));
+	ret.m_message_size				 = static_cast<unsigned int>(std::stod(umap.at("message_size")));
+	// エネルギーに関する情報
+	ret.m_initial_energy			 = std::stod(umap.at("initial_energy"));
+	ret.m_energy_consumption = EnergyConsumption(
+		std::stod(umap.at("Emov")), 
+		std::stod(umap.at("Eelec")),
+		std::stod(umap.at("Eamp"))
+	);
+	// 障害物に関する情報
+	ret.m_number_of_obstacles.first  = std::stoul(umap.at("minimum_number_of_obstacles"));
+	ret.m_number_of_obstacles.second = std::stoul(umap.at("maximum_number_of_obstacles"));
 	return ret;
 }
--- NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
+++ NetworkLibrary/SimpleWirelessNetwork/SWNConfig.h
@@ -4,31 +4,33 @@
 #include "geo.h"
 #include "Rail.h"
 #include "UIDCreator.h"
+#include "EnergyConsumption.h"
 
 namespace sim {
 	namespace swn {
 		class Config {
 		public:
-			// エリア幅
+			// エリア幅[m]
 			unsigned int m_area_width;
-			// エリア高さ
+			// エリア高さ[m]
 			unsigned int m_area_height;
 			// センサノード数
 			unsigned int m_number_of_sensor_nodes;
-			// 通信可能範囲
-			unsigned int m_transmission_range;
+			// レールの角度[radian]
+			std::vector<double> m_rail_angles;
+
+			// 通信可能範囲(最小、最大)[m]
+			std::pair<double, double> m_transmission_range;
 			// 1つのノードが収集するデータ数[bit]
 			unsigned int m_message_size;
+
 			// 初期エネルギー[J]
-			unsigned int m_initial_energy;
-			// 1bitのデータを通信する際の消費電力[J/bit]
-			double m_consume_energy_for_communication_per_bit;
-			// 1bitのデータを1mだけ送信する際に使用される増幅器の消費電力[J/(bit・m^2)]
-			double m_consume_energy_for_transmission_per_bit;
-			// レールの角度
-			std::vector<double> m_rail_angles;
-			// 障害物の位置
-			std::vector<std::vector<double>> m_obstacles_on_rails;
+			double m_initial_energy;
+			// 各主動作に必要な電力量
+			EnergyConsumption m_energy_consumption;
+
+			// 障害物の個数
+			std::pair<unsigned int, unsigned int> m_number_of_obstacles;
 			static Config createFromMap(const std::unordered_map<std::string, std::string>& umap);
 			// エリアの範囲を取得する
 			std::pair<unsigned int, unsigned int> getAreaRange() const {
@@ -42,6 +44,14 @@ namespace sim {
 					ret.push_back(Rail(uid_creator.get(), geo::StraightLine<double>::createFromSlopeAngleAndIntercept(rail_angle, 0.0)));
 				}
 				return ret;
+			}
+			// 通信可能範囲を選択する
+			double selectTransmissionRange(const double random_0_to_1) const {
+				return m_transmission_range.first + random_0_to_1 * (m_transmission_range.second - m_transmission_range.first);
+			}
+			// 障害物の個数を選択する
+			unsigned int selectNumberOfObstacles(const double random_0_to_1) const {
+				return static_cast<unsigned int>(m_number_of_obstacles.first + random_0_to_1 * (m_number_of_obstacles.second - m_number_of_obstacles.first));
 			}
 		};
 	}
